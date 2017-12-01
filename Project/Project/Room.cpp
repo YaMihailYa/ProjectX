f0fb2a9b83f583e1436686d63cd79786bc958dd5
YaMihailYa <mihailextra@gmail.com>
2017-12-01 19:58:04 +0200

    Rooms

--- Project/Project/Room.cpp
+++ Project/Project/Room.cpp
@@ -7,6 +7,13 @@ Room::Room(Room_t type, unsigned int id, sf::IntRect rect, bool enterable)
 {
 	m_left_room = nullptr;
 	m_right_room = nullptr;
+	m_top_room = nullptr;
+	m_down_room = nullptr;
+
+	m_left_door = nullptr;
+	m_right_door = nullptr;
+	m_top_door = nullptr;
+	m_down_door = nullptr;
 }
 
 
@@ -14,13 +21,12 @@ Room::~Room()
 {
 }
 
-sf::IntRect Room::move_player(sf::IntRect old_coords, sf::IntRect new_coords, Direction_t dir)
+sf::Vector2u Room::move_player(sf::Vector2u old_coords, sf::Vector2u new_coords, Direction_t dir)
 {
-	float new_center = ((float)new_coords.left) + new_coords.width / 2.0f;
 	if (dir == LEFT)
 	{
 		// If player get out of m_rect
-		if (new_center < m_rect.left)
+		if (new_coords.x < m_rect.left)
 		{
 			// If left room is connected and enterable
 			if (m_left_room != nullptr && m_left_room->Get_enterable())
@@ -30,7 +36,7 @@ sf::IntRect Room::move_player(sf::IntRect old_coords, sf::IntRect new_coords, Di
 			else
 			{
 				Player::Get()->Set_c_room(m_id);
-				old_coords.left = m_rect.left;
+				old_coords.x = m_rect.left;
 				return old_coords;
 			}
 		}
@@ -43,7 +49,7 @@ sf::IntRect Room::move_player(sf::IntRect old_coords, sf::IntRect new_coords, Di
 	else if (dir == RIGHT)
 	{
 		// If player get out of rect
-		if (new_center > m_rect.left + m_rect.width)
+		if (old_coords.x > m_rect.left + m_rect.width)
 		{
 			// If right room is connected and enterable
 			if (m_right_room != nullptr && m_right_room->Get_enterable())
@@ -53,7 +59,7 @@ sf::IntRect Room::move_player(sf::IntRect old_coords, sf::IntRect new_coords, Di
 			else
 			{
 				Player::Get()->Set_c_room(m_id);
-				old_coords.left = m_rect.left + m_rect.width - old_coords.width;
+				old_coords.x = m_rect.left + m_rect.width;
 				return old_coords;
 			}
 		}
