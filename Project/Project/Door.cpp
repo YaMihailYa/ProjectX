0618905062de9b320403eee1eb97cf4a60d4a20c
YaMihailYa <mihailextra@gmail.com>
2017-12-02 17:12:37 +0200

    Fix Door

--- Project/Project/Door.cpp
+++ Project/Project/Door.cpp
@@ -1,18 +1,24 @@
 #include "Door.h"
+#include "Player.h"
+#include "Render.h"
 
-#define Door_closed_id 0
-#define Door_closed_hovered_id 1
-#define Door_closed_background_id 2
-#define Door_opened_background_id 3
-
-Door::Door(Door_t type, bool is_closed, Colour_t key)
-	:m_type(type), m_is_closed(is_closed), m_key(key), m_background(nullptr), m_door(nullptr)
+Door::Door(unsigned int id, Door_t type, bool is_closed, Colour_t key, unsigned int cur_room_id,
+	unsigned int adj_room_id)
+	:m_type(type), m_is_closed(is_closed), m_key(key), m_background(nullptr), m_door(nullptr), m_pos(0, 0)
 {
 }
 
 
 Door::~Door()
 {
+	if (m_background != nullptr)
+		delete m_background;
+
+	if (m_door_handle != nullptr)
+		delete m_door_handle;
+
+	if (m_door != nullptr)
+		delete m_door;
 }
 
 void Door::display_background(sf::RenderWindow *window)
@@ -25,6 +31,11 @@ void Door::display_background(sf::RenderWindow *window)
 
 	if (m_is_closed)
 		m_door->display(window);
+}
+
+void Door::display_foreground(sf::RenderWindow *window)
+{
+
 }
 
 //void Door::Set_is_closed(bool is_closed)
@@ -60,5 +71,16 @@ void Door::display_background(sf::RenderWindow *window)
 
 float Door::get_center_coord()
 {
+	switch (m_type)
+	{
+	case DOOR_FRONT:
+		return m_pos.x*Render::Get()->Get_coef().x + m_background->Get_sprite().getGlobalBounds().width / 2;
+		break;
+	case DOOR_ROOMS:
+		return (m_pos.x + 75)*Render::Get()->Get_coef().x;
+		break;
+	default:
+		break;
+	}
 	return 0;
 }
--- Project/Project/Door.h
+++ Project/Project/Door.h
@@ -12,23 +12,31 @@ class Door
 {
 public:
 	/**
+	* @param id - id of door
 	* @param type - type of door
 	* @param is_closed - if door is closed
 	* @param key - colour of key to open door
+	* @param cur_room_id - room where door is set
+	* @param adj_room_id - adjacent room for this door
 	*/
-	Door(Door_t type, bool is_closed, Colour_t key);
+	Door(unsigned int id, Door_t type, bool is_closed, Colour_t key, unsigned int cur_room_id,
+		unsigned int adj_room_id);
 	~Door();
 
 	virtual void Set_is_closed(bool is_closed) = 0;
-	virtual void def_closed() = 0;
+	// pos without using coef
+	virtual void Set_pos(sf::Vector2u pos) = 0;
 
-	virtual void move_next() = 0;
+	// Control click
+	virtual void control() = 0;
 	
 	void display_background(sf::RenderWindow *window);
+	virtual void display_foreground(sf::RenderWindow *window);
 
-	void Set_door_pos(float x, float y) { m_door->setPosition(x, y); }
-	void Set_background(float x, float y) { m_background->setPosition(x, y); }
+	virtual void Set_door_pos(float x, float y) = 0;
+	//void Set_background(float x, float y) { m_background->setPosition(x, y); }
 
+	// With using coef
 	float get_center_coord();
 
 	virtual void go_throw_the_door() = 0;
@@ -37,6 +45,10 @@ public:
 
 
 protected:
+	unsigned int m_id;
+	unsigned int m_cur_room_id;
+	unsigned int m_adj_room_id;
+
 	Door_t m_type;
 
 	const Colour_t m_key;
@@ -45,5 +57,8 @@ protected:
 	Static_Object_Hovered* m_door;
 	Static_Object *m_background;
 	Static_Object *m_door_handle;
+
+	// Without using coef
+	sf::Vector2u m_pos;
 };
 
