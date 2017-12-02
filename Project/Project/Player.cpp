cbb81ab0fdc39a89b01ee2262f0fd4db097c1f24
YaMihailYa <mihailextra@gmail.com>
2017-12-02 11:57:06 +0200

    Add player

--- Project/Project/Player.cpp
+++ Project/Project/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+#include "Render.h"
 
 
 Player* Player::m_this = nullptr;
@@ -10,6 +11,7 @@ Player::Player(unsigned int speed)
 		exit(EXIT_FAILURE);
 	m_this = this;
 
+	sf::IntRect s;
 	// m_rect = 
 }
 
@@ -22,31 +24,46 @@ Player::~Player()
 
 void Player::display(sf::RenderWindow *window, unsigned int time)
 {
-	sf::IntRect old_rect, new_rect;
+	sf::Vector2u new_coords;
+	new_coords = m_c_pos;
+	Room* c_room = Render::Get()->Get_c_level()->Get_room_by_id(m_c_room_id);
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
-		
 		m_left_anim->animate(time);
-		old_rect = sf::IntRect(m_left_anim->Get_sprite().getGlobalBounds());
-		new_rect = old_rect;
-		new_rect.left += time*m_speed;
-		//new_rect = 
+		new_coords.x -= time*m_speed;
+		m_c_pos = c_room->move_player(m_c_pos, new_coords, Direction_t::LEFT);
+		m_left_anim->setPosition(m_c_pos.x, m_c_pos.y);
 		m_left_anim->display(window);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
 		m_right_anim->animate(time);
+		new_coords.x += time*m_speed;
+		m_c_pos = c_room->move_player(m_c_pos, new_coords, Direction_t::RIGHT);
+		m_right_anim->setPosition(m_c_pos.x, m_c_pos.y);
 		m_right_anim->display(window);
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+	else
+	{
+		m_stay_anim->animate(time);
+		m_stay_anim->setPosition(m_c_pos.x, m_c_pos.y);
+		m_stay_anim->display(window);
+	}
+	/* if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
 	{
 		m_stay_anim->animate(time);
 		m_stay_anim->display(window);
-	}
+	}*/
 }
 
-void Player::Move(Direction_t dir, unsigned int time)
+void Player::Move(/*Direction_t dir, unsigned int time*/)
 {
-	
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+	{
+		m_target_pos = sf::Vector2u(sf::Mouse::getPosition());
+		m_move_directions.clear();
+
+
+	}
 }
 
--- Project/Project/Player.h
+++ Project/Project/Player.h
@@ -13,12 +13,16 @@ public:
 	* @param dir direction
 	* @param time time from last call of function 
 	*/
-	void Move(Direction_t dir, unsigned int time);
+	void Move(/*Direction_t dir, unsigned int time*/);
+
+
+
 
 	void display(sf::RenderWindow *window, unsigned int time);
 
 	static Player* Get() { return m_this; }
 
+	void Set_c_pos(sf::Vector2u c_pos) { m_c_pos = c_pos; }
 	void Set_c_room(unsigned int id) { m_c_room_id = id; }
 	unsigned int Get_c_room() const { return m_c_room_id; }
 	
@@ -30,7 +34,13 @@ protected:
 	Animated_Object *m_left_anim;
 	Animated_Object *m_right_anim;
 	Animated_Object *m_stay_anim;
+
+
 	sf::Vector2u m_c_pos;
+	sf::Vector2u m_target_pos;
+	sf::Vector2u m_next_pos;
+	std::vector<Direction_t> m_move_directions;
+
 	sf::IntRect m_rect;
 	unsigned int m_speed; // pixels per MICROSECONDS
 
